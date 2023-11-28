6928f863c1cc64489d2274bc45ec5f5401d65be4
Orizon <vishalhero31@gmail.com>
2023-11-28 10:19:28 +0530

    added some mail envs

--- api/hello.js
+++ api/hello.js
@@ -6,8 +6,8 @@ export default function handler(req, res) {
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: "vishalhero31@gmail.com", // Change to your recipient
-    from: "test@example.com", // Change to your verified sender
+    to: process.env.FROM_MAIL, // Change to your recipient
+    from: process.env.TO_MAIL, // Change to your verified sender
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
